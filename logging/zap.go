916bb2224de4eaaad8b7efe57f79751906481cfb
Gianguido Sorà <me@gsora.xyz>
2021-04-15 14:56:37 +0200

    navigator-utils: print json structured logs in prod mode, do not create a log file if there isn't will for it

--- logging/zap.go
+++ logging/zap.go
@@ -14,14 +14,10 @@ type LoggingConfig struct {
 }
 
 // New creates a zap.SugaredLogger configured following lc's directives.
-// If lc fields are empty, a debug logger will be returned.
-// The logger returned by New is configured to automatically compress, rotate logs every 28 days or when
-// they reach 20 megabytes.
+// The non-debug logger returned by New is configured to output JSON-encoded structured logs to stdout.
+// If lc.LogPath is empty, no log file is created, otherwise it will be compressed and rotated every 20MB, or when it reaches
+// 28 days of usage. The last 3 copies are kept for backup.
 func New(lc LoggingConfig) *zap.SugaredLogger {
-	if lc == (LoggingConfig{}) {
-		lc.Debug = true
-	}
-
 	if lc.Debug {
 		// we can safely ignore the error here
 		dc, _ := zap.NewDevelopment()
@@ -30,27 +26,27 @@ func New(lc LoggingConfig) *zap.SugaredLogger {
 
 	var cores []zapcore.Core
 
-	l := &lumberjack.Logger{
-		Filename:   lc.LogPath,
-		MaxSize:    20,
-		MaxBackups: 3,
-		MaxAge:     28,
-		Compress:   true,
+	if lc.LogPath != "" {
+		l := &lumberjack.Logger{
+			Filename:   lc.LogPath,
+			MaxSize:    20,
+			MaxBackups: 3,
+			MaxAge:     28,
+			Compress:   true,
+		}
+
+		fileLogger := zapcore.AddSync(l)
+		cores = append(cores, zapcore.NewCore(
+			zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
+			fileLogger,
+			zap.InfoLevel,
+		))
 	}
 
-	fileLogger := zapcore.AddSync(l)
-	jsonWriter := zapcore.AddSync(os.Stdout)
-
+	stdWriter := zapcore.AddSync(os.Stdout)
 	cores = append(cores, zapcore.NewCore(
-		zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()),
-		fileLogger,
-		zap.InfoLevel,
-	))
-
-	// we use development encoder config in CLI output because it's easier to read
-	cores = append(cores, zapcore.NewCore(
-		zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig()),
-		jsonWriter,
+		zapcore.NewJSONEncoder(zap.NewDevelopmentEncoderConfig()),
+		stdWriter,
 		zap.InfoLevel,
 	))
 
