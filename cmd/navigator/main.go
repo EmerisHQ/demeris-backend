53123242d15b0876c9b946abdebe2dcdfdf423e5
Gianguido Sorà <me@gsora.xyz>
2021-04-19 17:51:30 +0200

    navigator-backend: initialize logger, database and router

--- cmd/navigator/main.go
+++ cmd/navigator/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"github.com/allinbits/navigator-utils/logging"
 
 	"github.com/allinbits/navigator-backend/config"
 	"github.com/allinbits/navigator-backend/database"
@@ -9,19 +9,26 @@ import (
 )
 
 func main() {
-
 	cfg, err := config.Read()
-
 	if err != nil {
 		panic(err)
 	}
 
-	if database.Init(cfg) != nil {
-		panic(err)
+	l := logging.New(logging.LoggingConfig{
+		Debug: cfg.Debug,
+	})
+
+	dbi, err := database.Init(cfg)
+	if err != nil {
+		l.Panicw("cannot initialize database", err)
 	}
 
-	defer database.Close()
+	r := router.New(
+		dbi,
+		l,
+	)
 
-	r := router.New()
-	log.Fatal(r.Run(":8080"))
+	if err := r.Serve(cfg.ListenAddr); err != nil {
+		l.Panicw("http server panic", err)
+	}
 }
