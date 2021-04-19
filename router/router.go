c61dec45622e0bab65bce2186869ac821f30c4ed
Gianguido Sorà <me@gsora.xyz>
2021-04-19 17:51:12 +0200

    navigator-backend: abstract router object, insert database and logging dependencies

--- router/router.go
+++ router/router.go
@@ -2,14 +2,35 @@ package router
 
 import (
 	"github.com/allinbits/navigator-backend/balances"
+	"github.com/allinbits/navigator-backend/database"
 	"github.com/allinbits/navigator-backend/trace"
+	"github.com/allinbits/navigator-utils/logging"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
-func New() *gin.Engine {
+type Router struct {
+	g  *gin.Engine
+	db *database.Database
+	l  *zap.SugaredLogger
+}
+
+func New(db *database.Database, l *zap.SugaredLogger) *Router {
 	router := gin.Default()
+
+	router.Use(logging.LogRequest(l.Desugar()))
+
 	registerRoutes(router)
-	return router
+
+	return &Router{
+		g:  router,
+		db: db,
+		l:  l,
+	}
+}
+
+func (r *Router) Serve(address string) error {
+	return r.g.Run(address)
 }
 
 func registerRoutes(engine *gin.Engine) {
