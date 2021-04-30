617686813495c208ebae7de6517bfbd55b0d332d
Gianguido Sorà <me@gsora.xyz>
2021-04-30 10:09:23 +0200

    cns: add fee address, move CounterpartyMapping as a general type

--- models/cns.go
+++ models/cns.go
@@ -7,14 +7,15 @@ import (
 
 // Chain represents CNS chain metadata row on the database.
 type Chain struct {
-	ID                uint64              `db:"id" json:"-"`
-	ChainName         string              `db:"chain_name" binding:"required" json:"chain_name"`                 // the unique name of the chain
-	CounterpartyNames CounterpartyMapping `db:"counterparty_names" binding:"required" json:"counterparty_names"` // a mapping of client_id to chain names used to identify which chain a given client_id corresponds to
-	NativeDenoms      DenomList           `db:"native_denoms" binding:"required" json:"native_denoms"`           // a list of denoms native to the chain
-	FeeTokens         DenomList           `db:"fee_tokens" binding:"required" json:"fee_tokens"`                 // a list of denoms accepted as fee on the chain, fee tokens must be verified
-	PriceModifier     float64             `db:"price_modifier" binding:"required" json:"price_modifier"`         // modifier (between 0 and 1) applied when estimating the price of a token hopping through the chain
-	BaseIBCFee        float64             `db:"base_ibc_fee" binding:"required" json:"base_ibc_fee"`             // average cost (in dollar) to submit an IBC transaction to the chain
-	GenesisHash       string              `db:"genesis_hash" binding:"required" json:"genesis_hash"`             // hash of the chain's genesis file
+	ID                uint64      `db:"id" json:"-"`
+	ChainName         string      `db:"chain_name" binding:"required" json:"chain_name"`                 // the unique name of the chain
+	CounterpartyNames DbStringMap `db:"counterparty_names" binding:"required" json:"counterparty_names"` // a mapping of client_id to chain names used to identify which chain a given client_id corresponds to
+	NativeDenoms      DenomList   `db:"native_denoms" binding:"required" json:"native_denoms"`           // a list of denoms native to the chain
+	FeeTokens         DenomList   `db:"fee_tokens" binding:"required" json:"fee_tokens"`                 // a list of denoms accepted as fee on the chain, fee tokens must be verified
+	FeeAddress        string      `db:"fee_address" binding:"required" json:"fee_address"`               // the address on which we accept fee payments
+	PriceModifier     float64     `db:"price_modifier" binding:"required" json:"price_modifier"`         // modifier (between 0 and 1) applied when estimating the price of a token hopping through the chain
+	BaseIBCFee        float64     `db:"base_ibc_fee" binding:"required" json:"base_ibc_fee"`             // average cost (in dollar) to submit an IBC transaction to the chain
+	GenesisHash       string      `db:"genesis_hash" binding:"required" json:"genesis_hash"`             // hash of the chain's genesis file
 	//nodeInfo nodeInfo // info required to query full-node (e.g. to submit tx)
 }
 
@@ -65,11 +66,11 @@ func (a *DenomList) Scan(value interface{}) error {
 	return json.Unmarshal(b, &a)
 }
 
-// CounterpartyMapping represent a mapping between IBC client IDs and chain names.
-type CounterpartyMapping map[string]string
+// DbStringMap represent a JSON database-enabled string map.
+type DbStringMap map[string]string
 
-// Scan is the sql.Scanner implementation for CounterpartyMapping.
-func (a *CounterpartyMapping) Scan(value interface{}) error {
+// Scan is the sql.Scanner implementation for DbStringMap.
+func (a *DbStringMap) Scan(value interface{}) error {
 	b, ok := value.([]byte)
 	if !ok {
 		return errors.New("type assertion to []byte failed")
