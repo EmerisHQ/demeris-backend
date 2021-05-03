6fca5bc2a0f010ce082308d5400d80e44dc5faf4
Gianguido Sorà <me@gsora.xyz>
2021-05-03 10:39:27 +0200

    api: query specific chain bech32 configuration

--- api/chains/api.go
+++ api/chains/api.go
@@ -10,9 +10,11 @@ import (
 
 func Register(router *gin.Engine) {
 	router.GET("/chains", GetChains)
+	router.GET("/chain/:chain", GetChain)
+	router.GET("/chain/:chain/bech32", GetChainBech32Config)
 }
 
-// GetVerifiedDenoms returns the list of all the chains supported by demeris.
+// GetChains returns the list of all the chains supported by demeris.
 func GetChains(c *gin.Context) {
 	var res chainsResponse
 
@@ -53,6 +55,101 @@ func GetChains(c *gin.Context) {
 	for _, cc := range chains {
 		res.SupportedChains = append(res.SupportedChains, cc.ChainName)
 	}
+
+	c.JSON(http.StatusOK, res)
+}
+
+// GetChain returns chain information by specifying its name.
+func GetChain(c *gin.Context) {
+	var res chainResponse
+
+	d, err := deps.GetDeps(c)
+	if err != nil {
+		c.Error(deps.NewError(
+			"chains",
+			fmt.Errorf("internal error"),
+			http.StatusInternalServerError,
+		))
+
+		panic("cannot retrieve context deps")
+		return
+	}
+
+	chainName := c.Param("chain")
+
+	chain, err := d.Database.Chain(chainName)
+
+	if err != nil {
+		e := deps.NewError(
+			"chains",
+			fmt.Errorf("cannot retrieve chain with name %v", chainName),
+			http.StatusBadRequest,
+		)
+
+		c.Error(e)
+
+		d.Logger.Errorw(
+			"cannot retrieve chain",
+			"id",
+			e.ID,
+			"name",
+			chainName,
+			"error",
+			err,
+		)
+
+		return
+	}
+
+	res.Chain = chain
+
+	c.JSON(http.StatusOK, res)
+}
+
+// GetChainBech32Config returns bech32 configuration for a chain by specifying its name.
+func GetChainBech32Config(c *gin.Context) {
+	var res bech32ConfigResponse
+
+	d, err := deps.GetDeps(c)
+	if err != nil {
+		c.Error(deps.NewError(
+			"chains",
+			fmt.Errorf("internal error"),
+			http.StatusInternalServerError,
+		))
+
+		panic("cannot retrieve context deps")
+		return
+	}
+
+	chainName := c.Param("chain")
+
+	chain, err := d.Database.Chain(chainName)
+
+	if err != nil {
+		e := deps.NewError(
+			"chains",
+			fmt.Errorf("cannot retrieve chain with name %v", chainName),
+			http.StatusBadRequest,
+		)
+
+		c.Error(e)
+
+		d.Logger.Errorw(
+			"cannot retrieve chain",
+			"id",
+			e.ID,
+			"name",
+			chainName,
+			"error",
+			err,
+		)
+
+		return
+	}
+
+	res.ChainName = chain.ChainName
+	res.Bech32Config = chain.NodeInfo.Bech32Config
 
 	c.JSON(http.StatusOK, res)
 }
--- api/chains/models.go
+++ api/chains/models.go
@@ -1,5 +1,16 @@
 package chains
 
+import "github.com/allinbits/demeris-backend/models"
+
 type chainsResponse struct {
 	SupportedChains []string `json:"supported_chains"`
+}
+
+type chainResponse struct {
+	Chain models.Chain `json:"chain"`
+}
+
+type bech32ConfigResponse struct {
+	ChainName    string              `json:"chain_name"`
+	Bech32Config models.Bech32Config `json:"bech32_config"`
 }
