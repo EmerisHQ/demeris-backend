1dff891c3b491f1189d6a25adce24472ac4a4a38
Gianguido Sorà <me@gsora.xyz>
2021-05-04 15:43:31 +0200

    tmwsproxy: move config out of main package

---
+++ tmwsproxy/config.go
@@ -0,0 +1,31 @@
+package tmwsproxy
+
+import (
+	"github.com/allinbits/demeris-backend/utils/configuration"
+	"github.com/allinbits/demeris-backend/utils/validation"
+	"github.com/go-playground/validator/v10"
+)
+
+type Config struct {
+	TendermintNode        string `validate:"required"`
+	ListenAddr            string `validate:"required"`
+	DatabaseConnectionURL string `validate:"required"`
+	Debug                 bool
+}
+
+func (c *Config) Validate() error {
+	err := validator.New().Struct(c)
+	if err == nil {
+		return nil
+	}
+
+	return validation.MissingFieldsErr(err, false)
+}
+
+func ReadConfig() (*Config, error) {
+	var c Config
+	return &c, configuration.ReadConfig(&c, "tmwsproxy", map[string]string{
+		"TendermintNode": "http://localhost:26657",
+		"ListenAddr":     "localhost:9999",
+	})
+}
