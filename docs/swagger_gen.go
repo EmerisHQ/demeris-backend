d70394252b9f83a4b76f6f1daa814d5af967b6fd
Gianguido Sorà <me@gsora.xyz>
2021-05-06 17:55:37 +0200

    chore: add swagger description, models swagger generation

--- docs/swagger_gen.go
+++ docs/swagger_gen.go
@@ -1,7 +1,21 @@
-//go:generate go run github.com/swaggo/swag/cmd/swag i -g ../docs/swagger_gen.go -d ../api --parseDependency -o ./
+//go:generate go run github.com/swaggo/swag/cmd/swag i -g ../docs/swagger_gen.go -d ../api --parseDepth 1 --parseDependency -o ./
 
+// @title Demeris API
+// @version 1.0
+// @description This is the Demeris backend API.
+
+// @contact.name API Support
+// @contact.email gautier@tendermint.com
+
+// @BasePath /
+// @query.collection.format multi
+
+// Package docs is needed to generate swagger documentation.
+// We keep underscore import here to make sure go mod doesn't remove swaggo dependency,
+// otherwise we cannot use the generate statement up there ^.
 package docs
 
-// We keep this import here to make sure go mod doesn't remove swaggo dependency,
-// otherwise we cannot use the generate statement up there ^.
-import _ "github.com/swaggo/swag"
+import (
+	_ "github.com/allinbits/demeris-backend/models"
+	_ "github.com/swaggo/swag"
+)
