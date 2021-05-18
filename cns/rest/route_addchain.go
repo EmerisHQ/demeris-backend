c98dd738fd064ae02ff528182ae2f450c2591ea6
Gianguido Sorà <me@gsora.xyz>
2021-05-18 13:06:12 +0200

    cns: /add only spins up a new chain when a NodeConfiguration struct is sent over

--- cns/rest/route_addchain.go
+++ cns/rest/route_addchain.go
@@ -15,7 +15,7 @@ const addChainRoute = "/add"
 type addChainRequest struct {
 	models.Chain
 
-	NodeConfig operator.NodeConfiguration `json:"node_config" binding:"required,dive"`
+	NodeConfig *operator.NodeConfiguration `json:"node_config"`
 }
 
 func (r *router) addChainHandler(ctx *gin.Context) {
@@ -27,19 +27,21 @@ func (r *router) addChainHandler(ctx *gin.Context) {
 		return
 	}
 
-	k := k8s.Querier{Client: *r.s.k}
+	if newChain.NodeConfig != nil {
+		k := k8s.Querier{Client: *r.s.k}
 
-	node, err := operator.NewNode(newChain.NodeConfig)
-	if err != nil {
-		e(ctx, http.StatusBadRequest, err)
-		r.s.l.Error("cannot add chain", err)
-		return
-	}
+		node, err := operator.NewNode(*newChain.NodeConfig)
+		if err != nil {
+			e(ctx, http.StatusBadRequest, err)
+			r.s.l.Error("cannot add chain", err)
+			return
+		}
 
-	if err := k.AddNode(*node); err != nil {
-		e(ctx, http.StatusInternalServerError, err)
-		r.s.l.Error("cannot add chain", err)
-		return
+		if err := k.AddNode(*node); err != nil {
+			e(ctx, http.StatusInternalServerError, err)
+			r.s.l.Error("cannot add chain", err)
+			return
+		}
 	}
 
 	if err := r.s.d.AddChain(newChain.Chain); err != nil {
