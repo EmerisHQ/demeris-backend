035d9a7552f79462aae49b7b54d3d75de043b15f
Gianguido Sorà <me@gsora.xyz>
2021-06-07 18:26:02 +0200

    chainwatch: spin up relayers when adding new chains

--- cns/chainwatch/chainwatch.go
+++ cns/chainwatch/chainwatch.go
@@ -1,7 +1,11 @@
 package chainwatch
 
 import (
+	"errors"
+	"fmt"
 	"time"
+
+	"github.com/allinbits/demeris-backend/utils/k8s/operator"
 
 	v1 "github.com/allinbits/starport-operator/api/v1"
 
@@ -42,25 +46,76 @@ func (i *Instance) Run() {
 
 		q := k8s.Querier{Client: i.k}
 
-		ns, err := q.ChainsByName(chains...)
+		var chainsNames []string
+		for _, cc := range chains {
+			chainsNames = append(chainsNames, cc.Name)
+		}
+
+		ns, err := q.ChainsByName(chainsNames...)
 		if err != nil {
 			i.l.Errorw("cannot get chains from k8s", "error", err)
 			continue
 		}
 
-		for _, n := range ns {
+		for idx, n := range ns {
 			if n.Status.Phase != v1.PhaseRunning {
 				i.l.Debugw("chain not in running phase", "name", n.Name, "phase", n.Status.Phase)
 				continue
 			}
 
-			i.l.Debugw("chain finished running", "name", n.Name)
-			if err := i.c.RemoveChain(n.Name); err != nil {
-				i.l.Errorw("cannot delete chain name from cache", "error", err)
-				continue
+			if err := i.chainFinished(chains[idx]); err != nil {
+				i.l.Errorw("cannot execute chain finished routine", "error", err)
 			}
-
-			// TODO: fetch denoms and write them into database
 		}
 	}
+}
+
+func (i *Instance) chainFinished(chain Chain) error {
+	// create relayers
+	if err := i.createRelayer(chain); err != nil {
+		return err
+	}
+
+	i.l.Debugw("chain finished running", "name", chain.Name)
+	if err := i.c.RemoveChain(chain); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (i *Instance) createRelayer(chain Chain) error {
+	q := k8s.Querier{Client: i.k}
+
+	cfg := operator.RelayerConfig{
+		NodesetName:   chain.Name,
+		AccountPrefix: chain.AddressPrefix,
+	}
+
+	if chain.HasFaucet {
+		cfg.FaucetName = fmt.Sprintf("%s-faucet", cfg.NodesetName)
+	}
+
+	relayerConfig, err := operator.BuildRelayer(cfg)
+	if err != nil {
+		return err
+	}
+
+	relayer, err := q.Relayer()
+	if err != nil && !errors.Is(err, k8s.ErrNotFound) {
+		return err
+	}
+
+	relayer.Spec.Chains = append(relayer.Spec.Chains, &relayerConfig)
+
+	var execErr error
+	if errors.Is(err, k8s.ErrNotFound) {
+		relayer.Namespace = "default"
+		relayer.ObjectMeta.Name = "relayer"
+		execErr = q.AddRelayer(relayer)
+	} else {
+		execErr = q.UpdateRelayer(relayer)
+	}
+
+	return execErr
 }
