aee1e949130b3f353a553870989415857f246d20
Brian Luk <brian6.dev@gmail.com>
2021-06-07 09:39:52 -0700

    management-ui: use /add endpoint

--- cns/admin/demeris-admin/pages/chains/_id.vue
+++ cns/admin/demeris-admin/pages/chains/_id.vue
@@ -1,47 +1,63 @@
 <template>
   <div class="container">
-
     <h1>{{ chain.chain_name }}</h1>
 
     <h3>Primary Channels</h3>
-
-    <div v-for="ch in Object.keys(chain.primary_channel)">
-      <label :for="ch">Primay Channel {{ ch }}</label>
-      <input
-        type="text"
-        :name="ch"
-        :id="ch"
-        v-model="chain.primary_channel[ch]"
-      />
-      <button v-on:click="updatePrimaryChannel(ch)">Update</button>
-
-    </div>
-
+    <table>
+      <thead>
+        <tr>
+          <th>Destination Chain</th>
+          <th>Primary Channel</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="ch in Object.keys(chain.primary_channel)">
+          <td>{{ ch }}</td>
+          <td>
+            <input
+              type="text"
+              :name="ch"
+              :id="ch"
+              v-model="chain.primary_channel[ch]"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <h3>Denoms</h3>
-    <div class="denoms" v-for="denom in chain.denoms">
 
-      <div class="denom">
-      {{ denom.name }}
-
-      <label :for="'denomDisplayName' + denom.name">Display Name</label>
-      <input
-        type="text"
-        :name="'denomDisplayName' + denom.name"
-        :id="'denomDisplayName' + denom.name"
-        v-model="denom.display_name"
-      />
-
-      <label :for="'verified' + denom.name">Verified</label>
-      <input
-        type="checkbox"
-        :name="'verified' + denom.name"
-        :id="'verified' + denom.name"
-        v-model="denom.verified"
-      />
-      </div>
-    </div>
-    <button v-on:click="updateDenoms()">Update</button>
-
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Display Name</th>
+          <th>Verified</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="denoms" v-for="denom in chain.denoms">
+          <td>{{ denom.name }}</td>
+          <td>
+            <input
+              type="text"
+              :name="'denomDisplayName' + denom.name"
+              :id="'denomDisplayName' + denom.name"
+              v-model="denom.display_name"
+            />
+          </td>
+          <td>
+            <input
+              type="checkbox"
+              :name="'verified' + denom.name"
+              :id="'verified' + denom.name"
+              v-model="denom.verified"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button v-on:click="update()">Save Changes</button>
+    <div class="error">{{ errorText }}</div>
     <!-- <h3>Raw data</h3>
     <p>
       {{ JSON.stringify(chain, "\n", 4) }}
@@ -56,47 +72,34 @@ export default {
       chain: {
         chain_id: "",
         primary_channel: {},
-        denoms: [] 
-      }
+        denoms: []
+      },
+      errorText: ""
     };
   },
   async created() {
-    console.log("loaded!");
-
-    await this.loadData()
+    await this.loadData();
   },
-    async mounted() {
-    console.log("loaded!");
-
-    await this.loadData()
+  async mounted() {
+    await this.loadData();
   },
 
   methods: {
-    async updatePrimaryChannel(dest_chain) {
-      let request = {
-        "chain_name": this.$route.params.id,
-        "dest_chain": dest_chain,
-        "primary_channel": this.chain.primary_channel[dest_chain]
-      }
-      console.log(JSON.stringify(request, "\n", 2));
-      await this.$axios.post("http://localhost:9999/update_primary_channel", request)
-      this.$nuxt.refresh()
-    },
-    async updateDenoms() {
-      let request = {
-        "chain_name": this.$route.params.id,
-        "denoms": this.chain.denoms
-      }
-      console.log(JSON.stringify(request, "\n", 2));
-      await this.$axios.post("http://localhost:9999/denoms", request)
-      this.$nuxt.refresh()
-    },
     async loadData() {
       let res = await this.$axios.get(
         "http://localhost:9999/chain/" + this.$route.params.id
       );
       console.log(res);
       this.chain = res.data.chain;
+    },
+    async update() {
+      let res = await this.$axios.post("http://localhost:9999/add", this.chain);
+      console.log(res);
+      if (res.status != 200) {
+        this.errorText = res.error;
+      } else {
+        this.$nuxt.refresh();
+      }
     }
   }
 };
@@ -104,11 +107,27 @@ export default {
 
 <style scoped>
 .denoms {
-  width: 100%
+  width: 100%;
 }
 .denom {
   width: 100%;
   margin: 10px;
   align-items: left;
+}
+
+th {
+  margin: 6px;
+  padding-right: 8px;
+}
+tr {
+  margin: 6px;
+  padding-right: 8px;
+}
+input {
+  margin: 6px;
+}
+
+.error {
+  color: red;
 }
 </style>
