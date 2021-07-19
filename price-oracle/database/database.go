47b52950c113fa49f8a2542a0b6063e12caea8b1
nodebreaker0-0 <zeroday26@gmail.com>
2021-07-19 23:04:13 +0900

    Price Oracle "CnsTokenQuery" SELECT RENAME COLUMN (diaplay_name->ticker)

--- price-oracle/database/database.go
+++ price-oracle/database/database.go
@@ -34,24 +34,24 @@ func New(connString string) (*Instance, error) {
 
 func CnsTokenQuery(db *sqlx.DB) ([]string, error) {
 	var Whitelists []string
-	q, err := db.Queryx("SELECT  y.x->'display_name',y.x->'fetch_price' FROM cns.chains jt, LATERAL (SELECT json_array_elements(jt.denoms) x) y")
+	q, err := db.Queryx("SELECT  y.x->'ticker',y.x->'fetch_price' FROM cns.chains jt, LATERAL (SELECT json_array_elements(jt.denoms) x) y")
 	if err != nil {
 		return nil, err
 	}
 	for q.Next() {
-		var display_name string
+		var ticker string
 		var fetch_price bool
-		err := q.Scan(&display_name, &fetch_price)
+		err := q.Scan(&ticker, &fetch_price)
 		if err != nil {
 			return nil, err
 		}
 		if fetch_price == true {
-			display_name = strings.TrimRight(display_name, "\"")
-			display_name = strings.TrimLeft(display_name, "\"")
-			if display_name[0:1] == "U" {
-				display_name = display_name[1:]
+			ticker = strings.TrimRight(ticker, "\"")
+			ticker = strings.TrimLeft(ticker, "\"")
+			if ticker[0:1] == "U" {
+				ticker = ticker[1:]
 			}
-			Whitelists = append(Whitelists, display_name)
+			Whitelists = append(Whitelists, ticker)
 		}
 	}
 	return Whitelists, nil
