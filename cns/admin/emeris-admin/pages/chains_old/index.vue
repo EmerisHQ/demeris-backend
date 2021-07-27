6b46eb70b1a663bb6843e6ba11031104cad56840
Brian Luk <brian6.dev@gmail.com>
2021-07-27 13:43:49 -0700

    admin-ui: move old admin ui files

---
+++ cns/admin/emeris-admin/pages/chains_old/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h1>CNS Chains</h1>
+    <div v-for="chain in chains">
+      {{ chain.chain_name || "no chains" }}
+      <router-link :to="chainlink(chain)"> Edit </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chains: []
+    };
+  },
+  methods: {
+    chainlink(chain) {
+      return "/chains/" + chain.chain_name;
+    }
+  },
+  async asyncData({ $axios, $config }) {
+    const { chains } = await $axios.$get("chains");
+    return { chains };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
