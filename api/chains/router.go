c66822639f557f6d9aa7e8a6b7912d4824edf981
JeseonLEE <zeroday26@gmail.com>
2021-07-30 02:50:25 +0530

    refactor chains api

---
+++ api/chains/router.go
@@ -0,0 +1,27 @@
+package chains
+
+import "github.com/gin-gonic/gin"
+
+const grpcPort = 9090
+
+func Register(router *gin.Engine) {
+	router.GET("/chains", GetChains)
+	router.GET("/chains/fee/addresses", GetFeeAddresses)
+
+	chain := router.Group("/chain/:chain")
+
+	chain.GET("", GetChain)
+	chain.GET("/denom/verify_trace/:hash", VerifyTrace)
+	chain.GET("/bech32", GetChainBech32Config)
+	chain.GET("/primary_channels", GetPrimaryChannels)
+	chain.GET("/primary_channel/:counterparty", GetPrimaryChannelWithCounterparty)
+	chain.GET("/status", GetChainStatus)
+	chain.GET("/supply", GetChainSupply)
+
+	fee := chain.Group("/fee")
+
+	fee.GET("", GetFee)
+	fee.GET("/address", GetFeeAddress)
+	fee.GET("/token", GetFeeToken)
+
+}
