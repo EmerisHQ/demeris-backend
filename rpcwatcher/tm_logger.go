0c2348519b94257a7e35dd694e43277b2b2f42fe
Gianguido Sorà <me@gsora.xyz>
2021-09-13 15:31:02 +0200

    rpcwatcher: properly transform tendermint log keyvals in something zap accepts

--- rpcwatcher/tm_logger.go
+++ rpcwatcher/tm_logger.go
@@ -10,16 +10,34 @@ type zapLogger struct {
 	chainName string
 }
 
+func getLogFields(chainName string, keyVals ...interface{}) []interface{} {
+	fields := []interface{}{
+		"chain_name",
+		chainName,
+	}
+
+	if len(keyVals)%2 != 0 {
+		return fields
+	}
+
+	for i := 0; i < len(keyVals); i += 2 {
+		fields = append(fields, keyVals[i].(string))
+		fields = append(fields, keyVals[i+1])
+	}
+
+	return fields
+}
+
 func (z zapLogger) Debug(msg string, keyvals ...interface{}) {
-	z.z.Debugw(msg, "chain_name", z.chainName, keyvals)
+	z.z.Debugw(msg, getLogFields(z.chainName, keyvals)...)
 }
 
 func (z zapLogger) Info(msg string, keyvals ...interface{}) {
-	z.z.Infow(msg, "chain_name", z.chainName, keyvals)
+	z.z.Infow(msg, getLogFields(z.chainName, keyvals)...)
 }
 
 func (z zapLogger) Error(msg string, keyvals ...interface{}) {
-	z.z.Errorw(msg, "chain_name", z.chainName, keyvals)
+	z.z.Errorw(msg, getLogFields(z.chainName, keyvals)...)
 }
 
 func (z zapLogger) With(_ ...interface{}) log.Logger {
