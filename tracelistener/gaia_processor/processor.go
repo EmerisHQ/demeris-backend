6aa4f1e1b9311dfa58c6262f07a09b9840426a30
Brian <45702419+lukitsbrian@users.noreply.github.com>
2021-10-12 02:20:27 -0700

    tracelistener: add validator tracking and endpoints (#215)
    * tracelistener: track updates to validators
    
    * api: add validator queries and endpoint
    
    * tracelistener: add delete operation
    
    * fix: missing brace at the end of validatorsResponse decl
    
    Co-authored-by: Gianguido Sorà <me@gsora.xyz>

--- tracelistener/gaia_processor/processor.go
+++ tracelistener/gaia_processor/processor.go
@@ -31,6 +31,7 @@ var defaultProcessors = []string{
 	"ibc_channels",
 	"ibc_connections",
 	"ibc_denom_traces",
+	"validators",
 }
 
 type Processor struct {
@@ -154,6 +155,12 @@ func processorByName(name string, logger *zap.SugaredLogger) (Module, error) {
 		return &authProcessor{
 			l:           logger,
 			heightCache: map[authCacheEntry]models.AuthRow{},
+		}, nil
+	case (&validatorsProcessor{}).ModuleName():
+		return &validatorsProcessor{
+			l:                     logger,
+			insertValidatorsCache: map[validatorCacheEntry]models.ValidatorRow{},
+			deleteValidatorsCache: map[validatorCacheEntry]models.ValidatorRow{},
 		}, nil
 	}
 }
