d3f9bf7edde9422b740aaa1fef622f4c5abe58eb
Brian <45702419+lukitsbrian@users.noreply.github.com>
2021-10-12 02:20:27 -0700

    tracelistener: add validator tracking and endpoints (#215)
    * tracelistener: track updates to validators
    
    * api: add validator queries and endpoint
    
    * tracelistener: add delete operation
    
    * fix: missing brace at the end of validatorsResponse decl
    
    This commit is a cherry-pick from master to have tracelistener44 build on the current branch.
    
    Contains some edits, namely the absence of /mint endpoints on api-server
    which we don't need since we're running it off master anyway.
    
    Co-authored-by: Gianguido Sorà <me@gsora.xyz>

--- api/chains/models.go
+++ api/chains/models.go
@@ -75,3 +75,26 @@ type statusResponse struct {
 type numbersResponse struct {
 	Numbers models.AuthRow `json:"numbers"`
 }
+
+type validatorsResponse struct {
+	Validators []models.ValidatorRow `json:"validators"`
+}
+
+type inflationResponse struct {
+	Inflation string `json:"inflation"`
+}
+
+type paramsResponse struct {
+	Params struct {
+		MintDenom           string `json:"mint_denom"`
+		InflationRateChange string `json:"inflation_rate_change"`
+		InflationMax        string `json:"inflation_max"`
+		InflationMin        string `json:"inflation_min"`
+		GoalBonded          string `json:"goal_bonded"`
+		BlocksPerYear       string `json:"blocks_per_year"`
+	} `json:"params"`
+}
+
+type annualProvisionsResponse struct {
+	AnnualProvisions string `json:"annual_provisions"`
+}
