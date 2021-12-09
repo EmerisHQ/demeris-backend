5717af77db695f9076ad2f1379260344a99dcbcd
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-12-09 17:30:18 +0530

    add ci script for /liquidity/node_info

---
+++ tests/liquidity/node_info_test.go
@@ -0,0 +1,59 @@
+package tests
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	utils "github.com/allinbits/demeris-backend/test_utils"
+)
+
+const (
+	baseUrl               = "%s://%s%s"
+	liquidityNodeEndpoint = "/liquidity/node_info"
+)
+
+func TestLiquidityStatus(t *testing.T) {
+	t.Parallel()
+
+	// arrange
+	env := os.Getenv("ENV")
+	emIngress, _ := utils.LoadIngressInfo(env, t)
+	chains := utils.LoadChainsInfo(env, t)
+	client := utils.CreateNetClient(env, t)
+
+	for _, ch := range chains {
+		if ch.Name == "cosmos-hub" {
+			t.Run(ch.Name, func(t *testing.T) {
+
+				// arrange
+				url := fmt.Sprintf(baseUrl+liquidityNodeEndpoint, emIngress.Protocol, emIngress.Host, emIngress.APIServerPath)
+				// act
+				resp, err := client.Get(url)
+				require.NoError(t, err)
+
+				// assert
+				require.Equal(t, http.StatusOK, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+
+				var values map[string]interface{}
+				utils.RespBodyToMap(resp.Body, &values, t)
+
+				fmt.Println(values["node_info"])
+				v, _ := values["node_info"].(map[string]interface{})
+				require.NotEmpty(t, v)
+				networkName := v["network"]
+
+				var fileResp map[string]interface{}
+				utils.StringToMap(ch.Payload, &fileResp, t)
+				fv, _ := fileResp["node_info"].(map[string]interface{})
+				require.NotEmpty(t, fv)
+				expectedName := fv["chain_id"]
+
+				require.Equal(t, expectedName, networkName)
+			})
+		}
+	}
+}
