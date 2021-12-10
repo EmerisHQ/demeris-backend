b12a727724ea99aa6e06fb2fb4bd822784d7e548
SpideyPool192 <krishna192reddy@gmail.com>
2021-12-10 15:12:27 +0530

    test for chain/XXX/bech32

---
+++ tests/chains/chain_bech32_test.go
@@ -0,0 +1,71 @@
+package tests
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/allinbits/demeris-backend-models/cns"
+	utils "github.com/allinbits/demeris-backend/test_utils"
+)
+
+const chainBech32Endpoint = "chain/%s/bech32"
+
+func TestChainBech32(t *testing.T) {
+	t.Parallel()
+
+	// arrange
+	env := os.Getenv("ENV")
+	emIngress, _ := utils.LoadIngressInfo(env, t)
+	chains := utils.LoadChainsInfo(env, t)
+	client := utils.CreateNetClient(env, t)
+
+	for _, ch := range chains {
+		t.Run(ch.Name, func(t *testing.T) {
+			// arrange
+			url := fmt.Sprintf(baseUrl+chainBech32Endpoint, emIngress.Protocol, emIngress.Host, emIngress.APIServerPath, ch.Name)
+			// act
+			resp, err := client.Get(url)
+			require.NoError(t, err)
+
+			// assert
+			if !ch.Enabled {
+				require.Equal(t, http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+			} else {
+				require.Equal(t, http.StatusOK, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+
+				var respValues map[string]interface{}
+				utils.RespBodyToMap(resp.Body, &respValues, t)
+
+				err = resp.Body.Close()
+				require.NoError(t, err)
+
+				data, err := json.Marshal(respValues["bech32_config"])
+				require.NoError(t, err)
+
+				var bech32 cns.Bech32Config
+				err = json.Unmarshal(data, &bech32)
+				require.NoError(t, err)
+
+				require.NotEmpty(t, bech32)
+
+				var payload map[string]interface{}
+				err = json.Unmarshal(ch.Payload, &payload)
+				require.NoError(t, err)
+
+				data, err = json.Marshal(payload["node_info"])
+				require.NoError(t, err)
+
+				var expectedNodeInfo cns.NodeInfo
+				err = json.Unmarshal(data, &expectedNodeInfo)
+				require.NoError(t, err)
+
+				require.Equal(t, expectedNodeInfo.Bech32Config.MainPrefix, bech32.MainPrefix)
+			}
+		})
+	}
+}
