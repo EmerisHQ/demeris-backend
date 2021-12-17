6d91952430aadec2d46024261bb21ac99c9f2c35
Stelios <sgerogia@gmail.com>
2021-12-17 13:16:42 +0000

    Fix logic of /status test

--- tests/chains/chain_status_test.go
+++ tests/chains/chain_status_test.go
@@ -37,12 +37,16 @@ func TestChainStatus(t *testing.T) {
 			require.NoError(t, err)
 
 			// assert
-			require.Equal(t, http.StatusOK, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+			if !ch.Enabled {
+				require.Equal(t, http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+			} else {
+				require.Equal(t, http.StatusOK, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
 
-			var values map[string]interface{}
-			utils.RespBodyToMap(resp.Body, &values, t)
+				var values map[string]interface{}
+				utils.RespBodyToMap(resp.Body, &values, t)
 
-			require.Equal(t, ch.Enabled, values[onlineKey].(bool), fmt.Sprintf("Chain %s Online %t", ch.Name, values[onlineKey].(bool)))
+				require.Equal(t, true, values[onlineKey].(bool), fmt.Sprintf("Chain %s Online %t", ch.Name, values[onlineKey].(bool)))
+			}
 		})
 	}
 }
