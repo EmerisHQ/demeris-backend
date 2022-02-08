286d372930a69b10ab391a9448249c98ea4f2248
akhilkumarpilli <akhilkumar7947@gmail.com>
2022-02-08 12:56:28 +0530

    Use test suite

--- tests/chains/chain_txs_test.go
+++ tests/chains/chain_txs_test.go
@@ -5,10 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"testing"
 	"time"
-
-	"github.com/stretchr/testify/require"
 
 	chainClient "github.com/allinbits/demeris-backend/chain_client"
 	utils "github.com/allinbits/demeris-backend/test_utils"
@@ -21,70 +18,66 @@ const (
 	randomTxHash     = "56FF608A76A01D9178039D17949F53ED8E3969752D546E5474605A67B13A42A0"
 )
 
-func TestTxsEndpoint(t *testing.T) {
-	// t.Parallel()
-
-	// arrange
-	env := "dev"
-	emIngress, _, err := utils.LoadIngressInfo(env)
-	require.NoError(t, err)
-	chains := utils.LoadClientChainsInfo(env, t)
-	client, err := utils.CreateNetClient(env)
-	require.NoError(t, err)
-
-	for _, ch := range chains {
-		t.Run(ch.Name, func(t *testing.T) {
+func (suite *testCtx) TestTxsEndpoint() {
+	for _, ch := range suite.clientChains {
+		suite.Run(ch.Name, func() {
 			var cc chainClient.Client
 			err := json.Unmarshal(ch.Payload, &cc)
-			require.NoError(t, err)
-			cli := chainClient.GetClient(t, env, ch.Name, cc)
+			suite.Require().NoError(err)
+			cli := chainClient.GetClient(suite.T(), suite.env, ch.Name, cc)
 			// assert
 			if !cli.Enabled {
 				// arrange
-				url := fmt.Sprintf(baseUrl+chainTxsEndpoint, emIngress.Protocol, emIngress.Host, emIngress.APIServerPath, ch.Name, randomTxHash)
+				url := fmt.Sprintf(baseUrl+chainTxsEndpoint, suite.emIngress.Protocol, suite.emIngress.Host, suite.emIngress.APIServerPath, ch.Name, randomTxHash)
 				// act
-				resp, err := client.Get(url)
-				require.NoError(t, err)
-				require.Equal(t, http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+				resp, err := suite.client.Get(url)
+				suite.Require().NoError(err)
+				suite.Require().Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
 				err = resp.Body.Close()
-				require.NoError(t, err)
+				suite.Require().NoError(err)
 			} else {
 				account, err := cli.AccountGet(cc.Key)
-				require.NoError(t, err)
+				suite.Require().NoError(err)
 
 				fromAddr, err := sdk.AccAddressFromBech32(account.Address)
-				require.NoError(t, err)
+				suite.Require().NoError(err)
 
 				account2, err := cli.AccountCreate("key2", "", cli.HDPath)
-				require.NoError(t, err)
+				suite.Require().NoError(err)
 
 				toAddr, err := sdk.AccAddressFromBech32(account2.Address)
-				require.NoError(t, err)
+				suite.Require().NoError(err)
 
+				// check balance
+				balance, err := cli.GetAccountBalances(account.Address, cli.Denom)
+				suite.Require().NoError(err)
+				suite.Require().True(balance.Amount.GT(sdk.NewInt(0)), "not enough balance in given account to perform tx")
+
+				// perform bank send tx
 				msg := banktypes.NewMsgSend(fromAddr, toAddr, sdk.NewCoins(sdk.NewCoin(cli.Denom, sdk.NewInt(100))))
-
 				txRes, err := cli.Broadcast(cc.Key, context.Background(), cli.GetContext(), msg)
-				require.NoError(t, err)
+				suite.Require().NoError(err)
 
 				hash := txRes.TxHash
 
 				time.Sleep(time.Second * 8)
 
 				// arrange
-				url := fmt.Sprintf(baseUrl+chainTxsEndpoint, emIngress.Protocol, emIngress.Host, emIngress.APIServerPath, ch.Name, hash)
+				url := fmt.Sprintf(baseUrl+chainTxsEndpoint, suite.emIngress.Protocol, suite.emIngress.Host, suite.emIngress.APIServerPath, ch.Name, hash)
 				// act
-				resp, err := client.Get(url)
-				require.NoError(t, err)
-				require.Equal(t, http.StatusOK, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+				resp, err := suite.client.Get(url)
+				suite.Require().NoError(err)
+				suite.Require().Equal(http.StatusOK, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
 
 				var respValues map[string]interface{}
-				utils.RespBodyToMap(resp.Body, &respValues, t)
+				utils.RespBodyToMap(resp.Body, &respValues, suite.T())
 
 				err = resp.Body.Close()
-				require.NoError(t, err)
+				suite.Require().NoError(err)
 
 				txhashMap := respValues["tx_response"].(map[string]interface{})
-				require.Equal(t, hash, txhashMap["txhash"])
+				suite.Require().NotEmpty(txhashMap)
+				suite.Require().Equal(hash, txhashMap["txhash"])
 			}
 		})
 	}
