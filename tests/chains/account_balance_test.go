5e6e40cd101775e879891e3416f3495a7cd7fd24
PrathyushaLakkireddy <prathyusha@vitwit.com>
2022-02-10 15:01:01 +0530

    test account balance endpoint

---
+++ tests/chains/account_balance_test.go
@@ -0,0 +1,60 @@
+package tests
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	chainClient "github.com/allinbits/demeris-backend/chain_client"
+	"github.com/allinbits/demeris-backend/models"
+	utils "github.com/allinbits/demeris-backend/test_utils"
+)
+
+const (
+	getBalanceEndpoint = "/account/%v/balance"
+)
+
+func (suite *testCtx) TestGetBalanceOfAnyAccount() {
+	suite.T().Parallel()
+
+	for _, ch := range suite.clientChains {
+		suite.Run(ch.Name, func() {
+			var cc chainClient.Client
+			err := json.Unmarshal(ch.Payload, &cc)
+			suite.Require().NoError(err)
+			cli := chainClient.GetClient(suite.T(), suite.env, ch.Name, cc)
+
+			hexAddress, err := cli.GetHexAddress(ch.Name)
+			suite.Require().NoError(err)
+			url := fmt.Sprintf(baseUrl+getBalanceEndpoint, suite.emIngress.Protocol, suite.emIngress.Host, suite.emIngress.APIServerPath, hexAddress)
+			// act
+			resp, err := suite.client.Get(url)
+			suite.Require().NoError(err)
+
+			if !cli.Enabled {
+				suite.Require().Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+				err = resp.Body.Close()
+				suite.Require().NoError(err)
+			} else {
+				suite.Require().Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+
+				var respValues map[string]interface{}
+				utils.RespBodyToMap(resp.Body, &respValues, suite.T())
+
+				err = resp.Body.Close()
+				suite.Require().NoError(err)
+				suite.Require().NotNil(respValues)
+
+				data, err := json.Marshal(respValues["balances"])
+				suite.Require().NoError(err)
+				suite.Require().NotNil(data)
+
+				var row models.BalanceRow
+				err = json.Unmarshal(data, &row)
+				suite.Require().NoError(err)
+				suite.Require().NotNil(row)
+				suite.Require().Equal(row.Denom, cli.Denom)
+			}
+		})
+	}
+}
