76d46561ee8bedc9bf3a2d996e7afc531204a6f3
Prathyusha Lakkireddy <prathyusha@vitwit.com>
2022-02-16 04:57:43 +0530

    test /chain/XXX/numbers/ADDRESS endpoint (#431)
    * add chain client
    
    * integrate get balance api
    
    * add  chains config
    
    * add multi chain config in create client
    
    * removed comments
    
    * removed client test file
    
    * fix lint issues
    
    * fix lint issues
    
    * fix issue
    
    * return bank bal response
    
    * add coin type
    
    * address review changes
    
    * modify client struct
    
    * add starport client struct
    
    * use starportclient's new method
    
    * address comments
    
    * address review comments
    
    * modify create account
    
    * add method to get single chain info
    
    * update chain client methods
    
    * updated client json files
    
    * remove import private key
    
    * load client
    
    * update go.mod
    
    * fix issue
    
    * supports v0.42 of sdk version
    
    * update go mod
    
    * add boradcast tx
    
    * fix lint
    
    * fix lint issues
    
    * build client ctx
    
    * fix tx issues
    
    * fix param name
    
    * set sdk config
    
    * Return broadcast response
    
    * add keyring backend const
    
    * updqate error msg
    
    * add denom in client
    
    * Add two get methods for client
    
    * add readme
    
    * Fix lint issues
    
    * Update test suite and modify getclient
    
    * fix issue
    
    * Handle fetch balance panic and use require in setup test suite
    
    * fix typos
    
    * get hexaddress method
    
    * Fix lint issue
    
    * test chain account numbers endpoint
    
    * add test suite
    
    * fix typo
    
    * fix typo
    
    * add conditions
    
    * address review comments
    
    Co-authored-by: akhilkumarpilli <akhilkumar7947@gmail.com>

---
+++ tests/chains/chain_account_numbers_test.go
@@ -0,0 +1,73 @@
+package tests
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	chainClient "github.com/allinbits/demeris-backend/chain_client"
+	"github.com/allinbits/demeris-backend/models"
+	utils "github.com/allinbits/demeris-backend/test_utils"
+)
+
+const (
+	chainNumbersEndpoint = "/chain/%s/numbers/%v"
+)
+
+func (suite *testCtx) TestGetChainNumbers() {
+	suite.T().Parallel()
+
+	for _, ch := range suite.clientChains {
+		suite.Run(ch.Name, func() {
+			var cc chainClient.Client
+			err := json.Unmarshal(ch.Payload, &cc)
+			suite.Require().NoError(err)
+			cli := chainClient.GetClient(suite.T(), suite.Env, ch.Name, cc)
+			suite.Require().NotNil(cli)
+
+			hexAddress, err := cli.GetHexAddress(cc.Key)
+			suite.Require().NoError(err)
+
+			urlPattern := strings.Join([]string{baseUrl, chainNumbersEndpoint}, "")
+			url := fmt.Sprintf(urlPattern, suite.EmIngress.Protocol, suite.EmIngress.Host, suite.EmIngress.APIServerPath, ch.Name, hex.EncodeToString(hexAddress))
+			// act
+			resp, err := suite.Client.Get(url)
+			suite.Require().NoError(err)
+
+			if !cli.Enabled {
+				suite.Require().Equal(http.StatusBadRequest, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+				err = resp.Body.Close()
+				suite.Require().NoError(err)
+
+				return
+			}
+			suite.Require().Equal(http.StatusOK, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+
+			var respValues map[string]interface{}
+			utils.RespBodyToMap(resp.Body, &respValues, suite.T())
+
+			err = resp.Body.Close()
+			suite.Require().NoError(err)
+			suite.Require().NotNil(respValues)
+
+			data, _ := json.Marshal(respValues["numbers"])
+			suite.Require().NotNil(data)
+
+			var row models.AuthRow
+			err = json.Unmarshal(data, &row)
+			suite.Require().NoError(err)
+			suite.Require().NotNil(row)
+
+			account, err := cli.AccountGet(cc.Key)
+			suite.Require().NoError(err)
+
+			suite.Require().Equal(ch.Name, row.ChainName)
+			suite.Require().Equal(account.Address, row.Address)
+
+			suite.Require().NotZero(row.AccountNumber)
+			suite.Require().NotZero(row.SequenceNumber)
+		})
+	}
+}
