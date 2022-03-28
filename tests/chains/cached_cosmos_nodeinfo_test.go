a2aa7d91a4fbc20f5424e932de32c7e5be6d1be7
SaReN <sahithnarahari@gmail.com>
2022-03-28 18:28:35 +0530

    chore: Improvements to chain client (#546)
    * chore: remove sdk config
    
    * rename client, rpc
    
    * rename node_addr to rpc
    
    * handle missed error
    
    * chore: code cleanup
    
    * chore: fix keyring issue
    
    * chore: remove keyring in chainclient
    
    * chore: update tests
    
    * chore: revert ci
    
    * chore: address comments
    
    * chore: remove skip for annual provisions

--- tests/chains/cached_cosmos_nodeinfo_test.go
+++ tests/chains/cached_cosmos_nodeinfo_test.go
@@ -18,47 +18,43 @@ const (
 
 func (suite *testCtx) TestCachedCosmosNodeinfo() {
 	for _, ch := range suite.clientChains {
-		if ch.Name == "cosmos-hub" {
-			suite.Run(ch.Name, func() {
-				var cc chainClient.Client
-				err := json.Unmarshal(ch.Payload, &cc)
-				suite.Require().NoError(err)
-				cli := chainClient.GetClient(suite.T(), suite.Env, ch.Name, cc)
-				suite.Require().NotNil(cli)
-
-				url := suite.Client.BuildUrl(cachedCosmosNodeEndpoint)
-				// act
-				resp, err := suite.Client.Get(url)
-				suite.Require().NoError(err)
-
-				suite.Require().Equal(http.StatusOK, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
-
-				data, err := ioutil.ReadAll(resp.Body)
-				suite.Require().NoError(err)
-
-				err = resp.Body.Close()
-				suite.Require().NoError(err)
-
-				if !cli.Enabled {
-					return
-				}
-				var nodeInfo tmservice.GetNodeInfoResponse
-				suite.Require().NoError(tmjson.Unmarshal(data, &nodeInfo))
-
-				//get cosmos nodeinfo
-				nodeInfoQuery := tmservice.NewServiceClient(cli.GetContext())
-				nodeInfoRes, err := nodeInfoQuery.GetNodeInfo(context.Background(), &tmservice.GetNodeInfoRequest{})
-				suite.Require().NoError(err)
-
-				n1, err := tmjson.Marshal(nodeInfo)
-				suite.Require().NoError(err)
-
-				n2, err := tmjson.Marshal(nodeInfoRes)
-				suite.Require().NoError(err)
-
-				// match result
-				suite.Require().Equal(string(n1), string(n2))
-			})
+		if ch.Name != "cosmos-hub" {
+			continue
 		}
+		suite.Run(ch.Name, func() {
+			var cc chainClient.ChainClient
+			err := json.Unmarshal(ch.Payload, &cc)
+			suite.Require().NoError(err)
+			cli, err := chainClient.GetClient(suite.Env, ch.Name, cc, suite.T().TempDir())
+			suite.Require().NoError(err)
+			suite.Require().NotNil(cli)
+
+			url := suite.Client.BuildUrl(cachedCosmosNodeEndpoint)
+			// act
+			resp, err := suite.Client.Get(url)
+			suite.Require().NoError(err)
+
+			suite.Require().Equal(http.StatusOK, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
+
+			data, err := ioutil.ReadAll(resp.Body)
+			suite.Require().NoError(err)
+
+			err = resp.Body.Close()
+			suite.Require().NoError(err)
+
+			if !cli.Enabled {
+				return
+			}
+			var nodeInfo tmservice.GetNodeInfoResponse
+			suite.Require().NoError(tmjson.Unmarshal(data, &nodeInfo))
+
+			//get cosmos nodeinfo
+			nodeInfoQuery := tmservice.NewServiceClient(cli.GetContext())
+			nodeInfoRes, err := nodeInfoQuery.GetNodeInfo(context.Background(), &tmservice.GetNodeInfoRequest{})
+			suite.Require().NoError(err)
+
+			// match result
+			suite.Require().Equal(nodeInfo.DefaultNodeInfo.Network, nodeInfoRes.DefaultNodeInfo.Network)
+		})
 	}
 }
