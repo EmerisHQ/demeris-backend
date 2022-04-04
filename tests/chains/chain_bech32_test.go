da9e035b2f9642c54480500e196cd760b7c6f366
PrathyushaLakkireddy <prathyusha@vitwit.com>
2022-04-04 11:03:12 +0530

    update bech32 ci script

--- tests/chains/chain_bech32_test.go
+++ tests/chains/chain_bech32_test.go
@@ -3,11 +3,12 @@ package tests
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"testing"
 
+	chainModels "github.com/allinbits/demeris-api-server/api/chains"
 	"github.com/allinbits/demeris-backend-models/cns"
-	utils "github.com/allinbits/demeris-backend/test_utils"
 )
 
 const chainBech32Endpoint = "chain/%s/bech32"
@@ -27,33 +28,23 @@ func (suite *testCtx) TestChainBech32() {
 			} else {
 				suite.Require().Equal(http.StatusOK, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
 
-				var respValues map[string]interface{}
-				utils.RespBodyToMap(resp.Body, &respValues, t)
+				data, err := ioutil.ReadAll(resp.Body)
+				suite.Require().NoError(err)
 
 				err = resp.Body.Close()
 				suite.Require().NoError(err)
 
-				data, err := json.Marshal(respValues["bech32_config"])
-				suite.Require().NoError(err)
-
-				var bech32 cns.Bech32Config
+				var bech32 chainModels.Bech32ConfigResponse
 				err = json.Unmarshal(data, &bech32)
 				suite.Require().NoError(err)
 
 				suite.Require().NotEmpty(bech32)
 
-				var payload map[string]interface{}
-				err = json.Unmarshal(ch.Payload, &payload)
+				var expectedNodeInfo cns.Chain
+				err = json.Unmarshal(ch.Payload, &expectedNodeInfo)
 				suite.Require().NoError(err)
 
-				data, err = json.Marshal(payload["node_info"])
-				suite.Require().NoError(err)
-
-				var expectedNodeInfo cns.NodeInfo
-				err = json.Unmarshal(data, &expectedNodeInfo)
-				suite.Require().NoError(err)
-
-				suite.Require().Equal(expectedNodeInfo.Bech32Config, bech32)
+				suite.Require().Equal(expectedNodeInfo.NodeInfo.Bech32Config, bech32.Bech32Config)
 			}
 		})
 	}
