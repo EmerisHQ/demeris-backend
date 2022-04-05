c5a8c33bfb5b5e30d6525921834f1c7548da6e8e
PrathyushaLakkireddy <prathyusha@vitwit.com>
2022-04-05 11:12:43 +0530

    update mint inflation

--- tests/chains/mint_inflation_test.go
+++ tests/chains/mint_inflation_test.go
@@ -1,11 +1,13 @@
 package tests
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"testing"
 
-	utils "github.com/allinbits/demeris-backend/test_utils"
+	chainModels "github.com/allinbits/demeris-api-server/api/chains"
 )
 
 const (
@@ -31,12 +33,17 @@ func (suite *testCtx) TestMintInflation() {
 			} else {
 				suite.Require().Equal(http.StatusOK, resp.StatusCode, fmt.Sprintf("Chain %s HTTP code %d", ch.Name, resp.StatusCode))
 
-				var respValues map[string]interface{}
-				utils.RespBodyToMap(resp.Body, &respValues, t)
+				data, err := ioutil.ReadAll(resp.Body)
+				suite.Require().NoError(err)
 
-				//expect a numeric value
-				inflation := respValues[inflationKey]
-				suite.Require().NotZero(inflation)
+				err = resp.Body.Close()
+				suite.Require().NoError(err)
+
+				var inflation chainModels.InflationResponse
+				err = json.Unmarshal(data, &inflation)
+				suite.Require().NoError(err)
+
+				suite.Require().NotZero(inflation.Inflation)
 			}
 		})
 	}
